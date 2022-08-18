7aace4ae697879bfbcfcad7b831a2f6207365a95
jenvasquez <jenvasquezh@gmail.com>
2022-08-17 22:17:37 -0500

    AppPresentación v1.1 - Se agrega integrante Jenifer

--- src/main/java/com/solvers/apppresentacion/Team.java
+++ src/main/java/com/solvers/apppresentacion/Team.java
@@ -7,8 +7,13 @@ public class Team {
         System.out.println();
 
         Teammate James = new Teammate("James Córdoba", 41, "Profesor", "Hacer música");
+        Teammate Jenifer = new Teammate("Jenifer Vasquez Hernandez", 38, "Analista de calidad", "Leer");
 
         System.out.println(James.getNombreApellido() + ", " + James.getEdad() + "años. ");
         System.out.println("Ocupación: " + James.getOcupacion() + "\nHobby: " + James.getHobby() + "\n----------------------- \n");
+
+        System.out.println(Jenifer.getNombreApellido() + ", " + Jenifer.getEdad() + "años. ");
+        System.out.println("Ocupación: " + Jenifer.getOcupacion() + "\nHobby: " + Jenifer.getHobby() + "\n----------------------- \n");
+
     }
 }
