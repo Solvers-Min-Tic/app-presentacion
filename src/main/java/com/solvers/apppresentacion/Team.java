6ee0334a54fab91294dcaaa8b007adcf033223b2
Julian Ruiz <juliandavidruiz.m@gmail.com>
2022-08-19 17:37:38 -0500

    Julian Ruiz

--- src/main/java/com/solvers/apppresentacion/Team.java
+++ src/main/java/com/solvers/apppresentacion/Team.java
@@ -12,6 +12,7 @@ public class Team {
         Teammate James = new Teammate("James Córdoba", 41, "Profesor", "Hacer música");
         Teammate Jenifer = new Teammate("Jenifer Vasquez Hernandez", 38, "Analista de calidad", "Leer");
         Teammate Rafa = new Teammate("Rafael Ahumada", 39, "Ing Sistemas", "Cantar");
+        Teammate Julian = new Teammate ("Julian Ruiz", 17, "Estudiante", "Escuchar musica");
 
         System.out.println(James.getNombreApellido() + ", " + James.getEdad() + "años. ");
         System.out.println("Ocupación: " + James.getOcupacion() + "\nHobby: " + James.getHobby() + "\n----------------------- \n");
@@ -21,7 +22,9 @@ public class Team {
         System.out.println();
         System.out.println(Rafa.getNombreApellido() + ", " + Rafa.getEdad() + "años. ");
         System.out.println("Ocupación: " + Rafa.getOcupacion() + "\nHobby: " + Rafa.getHobby() + "\n----------------------- \n");
-
+        System.out.println(Julian.getNombreApellido() +", "+ Julian.getEdad() + "años. ");
+        System.out.println("Ocupación: " + Julian.getOcupacion() + "\n Hobby:" + Julian.getHobby() + "\n-----------------------\n");
+        System.out.println();
 
     }
 }
