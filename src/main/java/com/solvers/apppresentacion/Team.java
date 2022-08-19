f7c913e9277e0ba31499acc1571d3a6ad6686260
RAFA AHUMADA <leafarsoft@gmail.com>
2022-08-18 20:46:21 -0500

    AppPresentación v1.2 - Se agrega integrante Rafael Ahumada

--- src/main/java/com/solvers/apppresentacion/Team.java
+++ src/main/java/com/solvers/apppresentacion/Team.java
@@ -4,17 +4,24 @@ public class Team {
 
 
     public static void main(String[] args) {
-        System.out.println("Este es el equipo de trabajo Solvers");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("* Este es el Equipo de Trabajo Solvers *");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
         System.out.println();
 
         Teammate James = new Teammate("James Córdoba", 41, "Profesor", "Hacer música");
         Teammate Jenifer = new Teammate("Jenifer Vasquez Hernandez", 38, "Analista de calidad", "Leer");
+        Teammate Rafa = new Teammate("Rafael Ahumada", 39, "Ing Sistemas", "Cantar");
 
         System.out.println(James.getNombreApellido() + ", " + James.getEdad() + "años. ");
         System.out.println("Ocupación: " + James.getOcupacion() + "\nHobby: " + James.getHobby() + "\n----------------------- \n");
-
+        System.out.println();
         System.out.println(Jenifer.getNombreApellido() + ", " + Jenifer.getEdad() + "años. ");
         System.out.println("Ocupación: " + Jenifer.getOcupacion() + "\nHobby: " + Jenifer.getHobby() + "\n----------------------- \n");
+        System.out.println();
+        System.out.println(Rafa.getNombreApellido() + ", " + Rafa.getEdad() + "años. ");
+        System.out.println("Ocupación: " + Rafa.getOcupacion() + "\nHobby: " + Rafa.getHobby() + "\n----------------------- \n");
+
 
     }
 }
